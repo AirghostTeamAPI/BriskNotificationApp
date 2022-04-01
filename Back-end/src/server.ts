800d9d8b02e8270c94ec381c92bbec198efa813d
Henrique <henrique.dousseau@dataside.com.br>
2022-04-01 20:26:51 -0300

    Adding Backend Starter Files

---
+++ Back-end/src/server.ts
@@ -0,0 +1,29 @@
+import bodyParser from "body-parser";
+import express from "express";
+import connectDB from "../config/database";
+import { importCsvFile } from './scripts/FOL';
+
+import FOL from './routes/api/FOL';
+
+const app = express();
+
+connectDB();
+
+app.set("port", process.env.PORT || 5000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/api", FOL);
+
+const port = app.get("port");
+const server = app.listen(port, () =>
+  console.log(`Server started on port ${port}`)
+);
+
+importCsvFile()
+setInterval(function () {
+  console.log('Syncing FOLs...');
+  importCsvFile()
+}, 60 * 5000); // 60 * 1000 milsec
+
+export default server;
