11d42cc42d9b6d58c7943c32eeb1002f505f42b6
unknown <ffelipe.ssilva@gmail.com>
2022-04-05 08:34:12 -0300

    Login files Creation

---
+++ Back-end/src/routes/api/User.ts
@@ -0,0 +1,16 @@
+import FOL from "../../models/User";
+import HttpStatusCodes from "http-status-codes";
+import { Request, Response, Router } from 'express';
+
+const router: Router = Router();
+
+router.get("/User", async (req: Request, res: Response) => {
+  try {
+    return res.status(HttpStatusCodes.OK).json(await FOL.find({ ...(req.query.search && { keywords: { "$regex": req.query.search, "$options": "i" } }), ...(req.query.equipment && { equipment: req.query.equipment }) }));
+  } catch (err) {
+    console.error((err as Error).message);
+    res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send("Server Error");
+  }
+});
+
+export default router;
