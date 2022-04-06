43e93603003f6687d70eeadff806787ec29ab8bb
Salitop <luizmiguelandre332@gmail.com>
2022-04-06 11:44:38 -0300

    Adicionando arquivos do projeto base para o backend

---
+++ Back-end/src/pages/Detail/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Image, ScrollView, TouchableOpacity, StyleSheet} from 'react-native';
+
+export default function Detail({ navigation }) {
+
+ return (
+   <ScrollView style={styles.container}>
+
+       <View>
+         <Text> Teste </Text>
+         </View>
+
+   </ScrollView>
+  );
+}
---
+++ Back-end/src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import  { useNavigation } from '@react-navigation/native';
+import { Button } from 'react-native-web';
+
+
+
+export default function Home() {
+ const navigation = useNavigation();
+
+ return (
+   <View>
+      <Text>Teste 2.1</Text>
+      <Button onClick={()=> navigation.navigate('Detail')}></Button>
+      </View>
+ )
+}
