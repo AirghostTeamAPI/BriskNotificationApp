34b6f12384b4424efc1900000b5ee9a3541df360
Gabriel Teixeira <61993394+Gabrieltg7@users.noreply.github.com>
2022-04-10 15:38:11 -0300

    create theme

--- Front-end/src/pages/Detail/index.js
+++ Front-end/src/pages/Detail/index.js
@@ -3,15 +3,21 @@ import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'rea
 import { MaterialIcons } from '@expo/vector-icons';
 import  { useNavigation } from '@react-navigation/native';
 import { Button } from 'react-native-web';
+import { useTheme } from 'react-native-paper';
 
 
 
 export default function Detail() {
  const navigation = useNavigation();
-
+ const {colors} = useTheme();
+ const styles = StyleSheet.create({
+  view: {
+    backgroundColor: colors.primary,
+  },
+});
  return (
    <View>
-      <Text>Detail</Text>
+      <Text style = {styles.view}>Detail</Text>
       <Button onPress={()=> navigation.navigate('Home')}></Button>
       </View>
  )
--- Front-end/src/router.js
+++ Front-end/src/router.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator} from '@react-navigation/stack';
 import { Feather } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
@@ -11,23 +12,37 @@ const Stack = createStackNavigator();
 
 function Routes(){
     return(
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                name="Home"
-                component={Home}
-                options={{ headerShown: false }}
-                />
-                <Stack.Screen 
-                name="Detail"
-                component={Detail}
-                options={{
-                    headerShown: false
-                }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <PaperProvider theme={theme}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{ headerShown: false }}
+                    />
+                    <Stack.Screen 
+                    name="Detail"
+                    component={Detail}
+                    options={{
+                        headerShown: false
+                    }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </PaperProvider>
     )
 }
 
 export default Routes;
+
+const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: '#E5E5E5',
+      accent: '#6EEBF0',
+      details: '#C4C4C4',
+      background: '#fff'  
+    },
+  };
