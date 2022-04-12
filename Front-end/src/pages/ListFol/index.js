aea8901b5723f8e53587613218102ff698bf1aeb
Gabriel Teixeira <61993394+Gabrieltg7@users.noreply.github.com>
2022-04-12 02:08:05 -0300

    Appbase (#6)
    * Adicionando arquivos do projeto base para o backend
    
    * Adicionando app base para o front-end
    
    * Configuração no router para que troque de página e botões na página Home e Details para navegação
    
    * Create theme (#2)
    
    * create theme
    
    * Adding main colors
    
    * Update router.js
    
    * Adding header (#3)
    
    * AddingHeader
    
    * Adding Vehicle Component
    
    * Adding user imports 
    
    * Adding new Components
    
    * Adding user login rotes
    
    * Adding Cors package
    
    * Removed console.log
    
    * Adding appbar itens
    
    Co-authored-by: Henrique <henrique.dousseau@dataside.com.br>
    
    * List fol (#5)
    
    * Adding page for listing FOLs by User's vehicle
    
    * Adding API connection
    
    Co-authored-by: Luiz.Andre <luiz.miguel@softwillians.com.br>
    
    * Front login (#4)
    
    * Add form login and remove files
    
    * Update login frontend
    
    * add api back to front
    
    * Update index.js
    
    * Merge Backend
    
    * Update back and login
    
    * Merge and fix goBack
    
    Co-authored-by: Salitop <luizmiguelandre332@gmail.com>
    Co-authored-by: Luiz.Andre <luiz.miguel@softwillians.com.br>
    Co-authored-by: Letícia Santos <62018632+LeticiaSan@users.noreply.github.com>
    Co-authored-by: Henrique <henrique.dousseau@dataside.com.br>
    Co-authored-by: Luiz Miguel <67153101+Salitop@users.noreply.github.com>

---
+++ Front-end/src/pages/ListFol/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity, Picker } from 'react-native';
+import  { useNavigation } from '@react-navigation/native';
+import { useTheme ,Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import CardFol from '../../Components/FOL';
+
+
+export default function ListFol() {
+ const navigation = useNavigation();
+ const {colors} = useTheme();
+ const [selectedValue, setSelectedValue] = useState("Todos");
+ const styles = StyleSheet.create({
+  view: {
+    backgroundColor: colors.primary,
+    color: colors.background
+  },
+});
+
+ return (
+   <View>
+	  <Picker
+        selectedValue={selectedValue}
+        style={{ height: 25, width: 150 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Todos" value="1" />
+        <Picker.Item label="Porsche" value="2" />
+		<Picker.Item label="Mercedes" value="3" />
+		<Picker.Item label="Corvette" value="4" />
+      </Picker>
+
+<CardFol/>
+
+    </View>
+ )
+}
+
---
+++ Front-end/src/pages/Login/index.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { Button, Title, Subheading, TextInput } from 'react-native-paper';
+import { StyleSheet, View, TextInput as Input, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useTheme } from 'react-native-paper';
+import api from "../../services/api";
+import  { useNavigation } from '@react-navigation/native';
+
+function Login (){
+  const navigation = useNavigation();
+
+  const signIn = async () => {
+    try {
+      const response = await api.post('/api/user/auth',{login: 'jpedro', password: '501358'});
+      const { user, token } = response.data;
+
+      await AsyncStorage.multiSet([
+        ['@CodeApi:token', token],
+        ['@CodeApi:user', JSON.stringify(user)],
+      ]);
+    } catch(err) {
+    }
+  };
+  const {colors} = useTheme();
+    const styles = StyleSheet.create({
+      TextInput: {
+        backgroundColor: "rgba(110, 235, 240, 0.3)",
+        opacity: "80%",
+        marginBottom: 15,
+        width: "70%",
+        alignSelf: "center",
+        color: "#fffff",
+        height: 50
+      },
+      Items: {
+        top: 5,
+        color: "#fff",
+      },
+      View:{
+        backgroundColor: colors.primary,
+        height: "100%",
+      },
+      Button: {
+        backgroundColor: "#ffffff",
+        width: "70%",
+        borderRadius: "30px",
+        alignSelf: "center",
+        color: "#5A55F2",
+      },
+      Text: {
+        fontSize: 20,
+        color: colors.background,
+        fontWeight: "bold",
+      },
+      TextButton: {
+        fontSize: 20,
+        color: colors.primary,
+        fontWeight: "bold",
+      },
+      Title: {
+        fontSize: 36,
+        color: "#ffffff",
+        fontFamily: 'tahoma',
+        fontStyle: "normal",
+        fontWeight: "bold",
+        paddingBottom: 30,
+      }
+    });
+      return(
+      <>
+      <View style = {styles.View}>
+        <center>
+        <Subheading style = { styles.Text }>Welcome to</Subheading><br/>
+        <Title style = { styles.Title }>B R I S K</Title>  </center>
+          <TextInput
+            mode="outlined"
+            label="Username"
+            placeholder="Type something"
+            theme={{ roundness: 30 }}
+            style = {styles.TextInput}
+            left={<TextInput.Icon name="account" style = {styles.Items} color = {colors.background}/>}
+          />
+          <TextInput
+            mode="outlined"
+            label="Password"
+            placeholder="Type something"
+            theme={{ roundness: 30 }}
+            style = {styles.TextInput}
+            left={<TextInput.Icon name="lock" style = {styles.Items} color = {colors.background}/>}
+          />
+        <Button icon="" mode="contained" onPress={()=> navigation.navigate('Home')} style = {styles.Button}>
+          <Text style = {styles.TextButton}>Sign in</Text>
+        </Button>
+      </View >
+      </>
+    );}
+
+    export default Login;
