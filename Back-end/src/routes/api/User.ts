3b610efbd4d6a901207468a603b1190f6a601cab
Henrique <henrique.dousseau@dataside.com.br>
2022-04-11 22:33:14 -0300

    Adicionando rotas de login do usuario

--- Back-end/src/routes/api/User.ts
+++ Back-end/src/routes/api/User.ts
@@ -1,12 +1,13 @@
-import FOL from "../../models/User";
 import HttpStatusCodes from "http-status-codes";
 import { Request, Response, Router } from 'express';
+import { authenticateUser } from '../../services/user';
 
 const router: Router = Router();
 
-router.get("/User", async (req: Request, res: Response) => {
+router.post("/user/auth", async (req: Request, res: Response) => {
   try {
-    return res.status(HttpStatusCodes.OK).json(await FOL.find({ ...(req.query.search && { keywords: { "$regex": req.query.search, "$options": "i" } }), ...(req.query.equipment && { equipment: req.query.equipment }) }));
+    const jwtToken = await authenticateUser(req.body.login, req.body.password)
+    return res.status(HttpStatusCodes.OK).json({ jwtToken });
   } catch (err) {
     console.error((err as Error).message);
     res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send("Server Error");
