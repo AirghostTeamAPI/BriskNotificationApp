f761d453c95deb455ab04b5699eab0090bad6c7c
Henrique <henrique.dousseau@dataside.com.br>
2022-04-11 22:33:14 -0300

    Adding user login routes

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -3,18 +3,41 @@ import express from "express";
 import connectDB from "../config/database";
 import { importCsvFile } from './scripts/FOL';
 import { importUserCsvFile } from './scripts/User';
-
-import FOL from './routes/api/FOL';
+import passport from 'passport';
+import BearerStrategy from 'passport-http-bearer';
+import User from "./models/User";
+import jwt from 'jsonwebtoken';
+import { IUser } from './interface/user';
+require('dotenv').config();
 
 const app = express();
 
 connectDB();
 
 app.set("port", process.env.PORT || 5000);
+app.set("jwt", process.env.jwtSecret || 5000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(passport.initialize());
+
+passport.use(new BearerStrategy(
+  function (token, done) {
+    const user = jwt.verify(token, process.env.jwtSecret) as IUser
+
+    User.findOne({ login: user.login }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      return done(null, user, { scope: 'all' });
+    });
+  }
+));
+
+import FOL from './routes/api/FOL';
+import USER from './routes/api/User';
+
 app.use("/api", FOL);
+app.use("/api", USER);
 
 const port = app.get("port");
 const server = app.listen(port, () =>
