0f661176ec34b0348cc0b369c0d913c81947f5bc
Luiz Miguel <67153101+Salitop@users.noreply.github.com>
2022-04-12 01:35:01 -0300

    List fol (#5)
    * Adding page for listing FOLs by User's vehicle
    
    * Adding API connection
    
    Co-authored-by: Luiz.Andre <luiz.miguel@softwillians.com.br>

---
+++ Front-end/src/Components/Fol/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity, Picker } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import  { useNavigation } from '@react-navigation/native';
+import { Button as ButtonWeb } from 'react-native-web';
+import { useTheme ,Avatar, Button, Card, Title, Paragraph, Colors } from 'react-native-paper';
+import api from "../../services/api";
+
+export default function CardFol() {
+
+ const {colors} = useTheme();
+    const styles = StyleSheet.create({
+        view: {
+          backgroundColor: colors.details 
+            
+        },
+      });
+
+
+  const listByEquipment = async () => 
+  { 
+    
+    try {            
+      const response = await api.get('/api/fols', {equipment: 'Porsche'});
+      const { equipment, title, issue_description} = response.data;
+      console.log(response);
+      await AsyncStorage.multiSet([
+       ['@CodeApi:equipment', JSON.stringify(equipment)],
+       ['@CodeApi:title', JSON.stringify(title)],
+       ['@CodeApi:issue_description', JSON.stringify(issue_description)],
+     ])
+ 
+     } catch(err) {
+     }
+  };
+  
+    return(
+      <Card>
+      <Card.Title
+       title= {listByEquipment.issue_description} subtitle={"•" + listByEquipment.title + " •" + listByEquipment.equipment} style = {styles.view}
+     
+     
+      subtitleStyle = {{color: colors.secondary}}
+      />
+       <Button onPress={listByEquipment}> </Button>
+     </Card>
+    
+  );
+}
+
--- Front-end/src/router.js
+++ Front-end/src/router.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator} from '@react-navigation/stack';
-import { Feather } from '@expo/vector-icons';
-import { TouchableOpacity } from 'react-native';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import Home from './pages/Home';
-import Detail from './pages/Detail';
+import ListFol from './pages/ListFol';
 import Header from './Components/appBar';
 
 const Stack = createStackNavigator();
@@ -22,9 +20,9 @@ function Routes(){
                 options={{ headerShown: true }}
                 />
                 <Stack.Screen 
-                name="Detail"
-                component={Detail}
-                options={{ headerShown: true }}
+                name="ListFol"
+                component={ListFol}
+                options={{ headerShown: false }}
                 />
             </Stack.Navigator>
         </NavigationContainer>
@@ -41,7 +39,8 @@ const theme = {
       ...DefaultTheme.colors,
       primary: '#5A55F2',
       accent: '#6EEBF0',
-      details: '#C4C4C4',
-      background: '#ffffff'  
+      details: '#E4E4E4',
+      background: '#ffffff',
+      secondary: '#18BDC3'
     },
   };
