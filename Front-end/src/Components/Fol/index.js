f72cf9dd3d4442d11e5264d06ba7174d3bca5d2a
Salitop <luizmiguelandre332@gmail.com>
2022-04-13 09:13:31 -0300

    adding integration by axios in fol list

--- Front-end/src/Components/Fol/index.js
+++ Front-end/src/Components/Fol/index.js
@@ -1,9 +1,6 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity, Picker } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
-import  { useNavigation } from '@react-navigation/native';
-import { Button as ButtonWeb } from 'react-native-web';
-import { useTheme ,Avatar, Button, Card, Title, Paragraph, Colors } from 'react-native-paper';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { useTheme , Button, Card } from 'react-native-paper';
 import api from "../../services/api";
 
 export default function CardFol() {
@@ -19,31 +16,53 @@ export default function CardFol() {
 
   const listByEquipment = async () => 
   { 
+    const jwt = require("jsonwebtoken");
+
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNTM5M2YzYWQxMzg4NmQ1Y2QzMzc3OCIsInVzZXJuYW1lIjoiSm_Do28gUGVkcm8iLCJlcXVpcG1lbnQiOiJNZXJjZWRlcywgQk1XLCBQb3JzY2hlIiwibG9naW4iOiJqcGVkcm8iLCJpYXQiOjE2NDk4MDk2NDl9.Q22NkF2IXl6Xa8oAeEXaMshK3Cx5jurT3AD0BgSduNs";
+  
+    const decoded = jwt.decode(token);
+    const decodedEquipament = decoded.equipment;
+    const stringDecodedEquipament = decodedEquipament.toString();
+    const listEquipment = stringDecodedEquipament.split(",");
     
-    try {            
-      const response = await api.get('/api/fols', {equipment: 'Porsche'});
-      const { equipment, title, issue_description} = response.data;
-      await AsyncStorage.multiSet([
-       ['@CodeApi:equipment', JSON.stringify(equipment)],
-       ['@CodeApi:title', JSON.stringify(title)],
-       ['@CodeApi:issue_description', JSON.stringify(issue_description)],
-     ])
- 
+    try {  
+      if(listEquipment.length > 1){
+        for (let i = 0; i < listEquipment.length; i++) {
+          const response = await api.get('/api/fols', {equipment: listEquipment[i]});
+          const { equipment, title, issue_description} = response.data;
+          return(
+           <Card>
+            <Card.Title
+             title= {'Inoperative Clutch Clarification'} subtitle={"•MRC-003/10 •Mercedes"} style = {styles.view}
+
+            subtitleStyle = {{color: colors.secondary}}
+            />
+             <Button onPress={listByEquipment}> </Button>
+           </Card>
+          
+          );
+        } 
+        
+      }    
+      else{
+        const response = await api.get('/api/fols', {equipment: listEquipment});
+        axios.get('/api/fols', { headers: { Authorization: token } })
+  .then(response => { console.log(response.data) })
+        const { equipment, title, issue_description} = response.data;
+
+        return(
+          <Card>
+          <Card.Title
+           title= {'Inoperative Clutch Clarification'} subtitle={"•MRC-003/10 •Mercedes"} style = {styles.view}
+          subtitleStyle = {{color: colors.secondary}}
+          />
+           <Button onPress={listByEquipment}> </Button>
+         </Card>
+        
+      );
+      }      
      } catch(err) {
      }
   };
-  
-    return(
-      <Card>
-      <Card.Title
-       title= {'Inoperative Clutch Clarification'} subtitle={"•MRC-003/10 •Mercedes"} style = {styles.view}
-     
-     
-      subtitleStyle = {{color: colors.secondary}}
-      />
-       <Button onPress={listByEquipment}> </Button>
-     </Card>
-    
-  );
 }
 
