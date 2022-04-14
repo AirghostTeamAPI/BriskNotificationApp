89fe2753ba52e85595534f23a7fb7a8feea4549f
Luiz.Andre <luiz.miguel@softwillians.com.br>
2022-04-14 20:33:44 -0300

    Adding equipments on Picker and listing fols by equipment

--- Front-end/src/pages/ListFol/index.js
+++ Front-end/src/pages/ListFol/index.js
@@ -3,7 +3,6 @@ import { View, StyleSheet, Picker } from 'react-native';
 import  { useNavigation } from '@react-navigation/native';
 import { useTheme } from 'react-native-paper';
 import CardFol from '../../Components/FOL';
-import api from "../../services/api";
 import Axios from 'axios';
 
 export default function ListFol({route}) {
@@ -11,7 +10,6 @@ export default function ListFol({route}) {
  const navigation = useNavigation();
  const {colors} = useTheme();
  const [value, setValue] = useState();
- const [selectedValue, setSelectedValue] = useState("Todos");
  const styles = StyleSheet.create({
   picker:{
     marginLeft: "2%",
@@ -24,30 +22,35 @@ export default function ListFol({route}) {
     width: "150px"
   }
 });
- 
 const jwt = require("jsonwebtoken");
 const token = route.params.token;
 const decoded = jwt.decode(token);
 const decodedEquipament = decoded.equipment;
 const stringDecodedEquipament = decodedEquipament.toString();
-const listEquipment = stringDecodedEquipament.split(", ");  
+const listEquipment = stringDecodedEquipament.split(", "); 
 
 React.useEffect(() => {
-   Axios.get(`http://localhost:5000/api/fols/?equipment=${listEquipment[0]}`, {headers: {
+  Axios.get(`http://localhost:5000/api/fols/?equipment=${listEquipment[0]}`, {headers: {
+    "Authorization": `Bearer ${token}`}}).then((response)=>
+  {setValue(response.data)});
+},[]);
+
+function listFolBySelectedEquipment(selectedValue){
+  Axios.get(`http://localhost:5000/api/fols/?equipment=${selectedValue}`, {headers: {
       "Authorization": `Bearer ${token}`}}).then((response)=>
     {setValue(response.data)});
-  },[]);
+   
+}
+
 return (
   <View>
    <Picker
-       selectedValue={selectedValue}
        style = {styles.picker}
-       onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+       onValueChange={(itemValue) => (listFolBySelectedEquipment(itemValue))}
      >
-       <Picker.Item label="Todos" value="1" />
-       <Picker.Item label="Porsche" value="2" />
-        <Picker.Item label="Mercedes" value="3" />
-        <Picker.Item label="Corvette" value="4" />
+       {
+        listEquipment.map((eq) => <Picker.Item label={eq} value={eq} />)
+      }
      </Picker>
         {
            value?.map((linha)=><CardFol linha={linha}/>)
@@ -55,4 +58,3 @@ return (
       </View>
   )
 }
-
