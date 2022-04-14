cc85cbd1a12ebc16e17a3d46bff42440e0fc8817
LeticiaSan <leticia.santos74@fatec.sp.gov.br>
2022-04-14 01:56:13 -0300

    Update index.js

--- Front-end/src/pages/Home/index.js
+++ Front-end/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
 import CardVehicle from '../../Components/vehicle';
 
 
