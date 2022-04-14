a498f70613aa72814ee73244aad5fb31e212f1ee
LeticiaSan <leticia.santos74@fatec.sp.gov.br>
2022-04-14 11:26:28 -0300

    Updating Component Fol theme

--- Front-end/src/Components/Fol/index.js
+++ Front-end/src/Components/Fol/index.js
@@ -5,28 +5,25 @@ import api from "../../services/api";
 import axios from  'axios'
 
 export default function CardFol(props) {
+  const title = props.linha.title;
+  const equipment = props.linha.equipment
+  const description = props.linha.issue_description;
 
  const {colors} = useTheme();
     const styles = StyleSheet.create({
-        view: {
-          backgroundColor: colors.details   
-        },
         card:{
           marginLeft: "2%",
           marginRight: "2%",
           marginTop: "1%",
           borderLeftWidth: "5px",
           borderLeftColor: colors.accent,
-          backgroundColor: colors.background,
-          
+          backgroundColor: colors.background,          
         }
       });
   return(
     <Card style = {styles.card}>
-      <Card.Title title= 'teste' subtitle='• Teste • Teste' 
-      style = {styles.view} subtitleStyle = {{color: colors.secondary, fontWeight: "bold"}} 
-     // title= {prop.issue_description} subtitle={"• " + props.equipment + " • " + prop.title} style = {styles.view}
-     //subtitleStyle = {{color: colors.secondary}}   
+      <Card.Title title= {description} subtitle= {`• ${title} • ${equipment}`} 
+       subtitleStyle = {{color: colors.secondary, fontWeight: "bold"}}  
      />
     </Card>
    );
