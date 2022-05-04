9c9453c79ec3635d87b999863d2d54fb8bb96cfd
LeticiaSan <leticia.santos74@fatec.sp.gov.br>
2022-05-04 08:53:15 -0300

    adding pushNotificationOnFront

--- Front-end/App.js
+++ Front-end/App.js
@@ -1,29 +1,57 @@
 import 'react-native-gesture-handler';
 
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { AppLoading } from 'expo';
-import { useFonts, Anton_400Regular} from '@expo-google-fonts/anton';
+import { useFonts, Anton_400Regular } from '@expo-google-fonts/anton';
 import { Provider as PaperProvider } from 'react-native-paper';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { Text, View, Button, Platform } from 'react-native';
+
 
 import Routes from './src/router';
 
 export default function App() {
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+
 
   let [fontsLoaded] = useFonts({
     Anton_400Regular,
   });
 
 
-  if(!fontsLoaded){
+  if (!fontsLoaded) {
     return <AppLoading />;
   }
 
   return (
     <>
-     <PaperProvider>
+      <PaperProvider>
         <StatusBar style="light" backgroundColor="#000" translucent={true} />
-        <Routes/>
+        <Routes />
       </PaperProvider>
     </>
   );
