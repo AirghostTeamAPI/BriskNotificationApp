c870ee15ad81c91133a536dbec389d6dbb5aba1e
Gabriel Teixeira <61993394+Gabrieltg7@users.noreply.github.com>
2022-05-15 18:03:22 -0300

    Update index.js

--- Front-end/src/Components/Fol/index.js
+++ Front-end/src/Components/Fol/index.js
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
+
 import { useNavigation } from '@react-navigation/native';
 import Axios from 'axios';
 import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { useTheme, Card } from 'react-native-paper';
-=======
-import React, { useState } from 'react';
+
 import { StyleSheet } from 'react-native';
 import { useTheme ,Avatar, Button, Card, Title, Paragraph, Colors } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/Entypo';
->>>>>>> b2b32b001b23676742aa0f390b77165fbcc57f1f
+
 
 export default function CardFol(props) {
   console.log(props.linha.ifViewed)
@@ -17,30 +16,6 @@ export default function CardFol(props) {
   const title = props.linha.title;
   const equipment = props.linha.equipment
   const description = props.linha.issue_description;
-<<<<<<< HEAD
-  const navigation = useNavigation();
-  const [pageNumber, setPage] = useState();
-
-
-  const { colors } = useTheme();
-  const styles = StyleSheet.create({
-    card: {
-      marginLeft: "2%",
-      marginRight: "2%",
-      marginTop: "1%",
-      borderLeftWidth: 5,
-      borderLeftColor: colors.accent,
-      backgroundColor: colors.background,
-    }
-  });
-  return (
-    <Card key={title} onPress={() => { navigation.navigate("viewFol", { title: title }) }} style={styles.card}>
-      <Card.Title title={description} subtitle={`• ${title} • ${equipment}`}
-        subtitleStyle={{ color: colors.secondary, fontWeight: "bold" }}
-      />
-    </Card>
-  );
-=======
   const {colors} = useTheme();
   const styles = StyleSheet.create
   ({
@@ -85,6 +60,5 @@ export default function CardFol(props) {
       </Card>
      );
     }
->>>>>>> b2b32b001b23676742aa0f390b77165fbcc57f1f
 }
 
