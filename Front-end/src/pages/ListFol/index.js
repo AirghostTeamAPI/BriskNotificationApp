81c9f886f2f0460ef779a357d395afbbbac9cfc1
Gabriel Teixeira <61993394+Gabrieltg7@users.noreply.github.com>
2022-05-15 18:06:07 -0300

    Update index.js

--- Front-end/src/pages/ListFol/index.js
+++ Front-end/src/pages/ListFol/index.js
@@ -1,53 +1,5 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Picker } from 'react-native';
-<<<<<<< HEAD
-import { useNavigation } from '@react-navigation/native';
-import { useTheme } from 'react-native-paper';
-import CardFol from '../../Components/Fol'
-import Axios from 'axios';
-import Header from '../../Components/appBar';
-
-export default function ListFol({ route }) {
-
-  const navigation = useNavigation();
-  const { colors } = useTheme();
-  const [value, setValue] = useState();
-  const styles = StyleSheet.create({
-    picker: {
-      marginLeft: "2%",
-      marginRight: "2%",
-      marginTop: "1%",
-      height: "25px",
-      borderColor: colors.accent,
-      borderWidth: "1px",
-      borderRadius: 5,
-      width: "150px"
-    }
-  });
-  const jwt_decode = require("jwt-decode");
-  const token = route.params.token;
-  const selectedEquipmentParam = route.params.selectedEquipmentParam;
-  const decoded = jwt_decode(token);
-  const decodedEquipament = decoded.equipment;
-  const stringDecodedEquipament = decodedEquipament.toString();
-  const listEquipment = stringDecodedEquipament.split(", ");
-  const selectedEquipment = selectedEquipmentParam.trim();
-
-  React.useEffect(() => {
-    Axios.get(`http://localhost:5000/api/fols/?equipment=${selectedEquipment}`, {
-      headers: {
-        "Authorization": `Bearer ${token}`
-      }
-    }).then((response) => { setValue(response.data) });
-  }, []);
-
-  function listFolBySelectedEquipment(selectedValue) {
-    Axios.get(`http://localhost:5000/api/fols/?equipment=${selectedValue}`, {
-      headers: {
-        "Authorization": `Bearer ${token}`
-      }
-    }).then((response) => { setValue(response.data) });
-=======
 import  { useNavigation } from '@react-navigation/native';
 import { useTheme, Searchbar } from 'react-native-paper';
 import CardFol from '../../Components/FOL';
